26db226018c24830a8fc1289631e51762cb386f4
Koromore <7639745979@qq.com>
2019-10-12 13:04:52 +0800

    3D

---
+++ src/page/index/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="index">
+    <index-content></index-content>
+  </div>
+</template>
+
+<script>
+import IndexContent from './components/content'
+export default {
+  name: 'Content',
+  // 组件注册
+  components: {
+    IndexContent
+  },
+  // 数据
+  data () {
+    return {}
+  },
+  // 事件
+  methods: {},
+  // 计算属性
+  computed: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+
+</style>
